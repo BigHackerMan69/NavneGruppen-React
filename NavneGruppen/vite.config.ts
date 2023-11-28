205d8b62758ed67371c88a7dc45d112aa54b7890
Swaiidan <ibra0529@easv.dk>
2023-11-28 22:55:14 +0100

    ændert

--- NavneGruppen/vite.config.ts
+++ NavneGruppen/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173, // Sæt det ønskede portnummer
+    open: '/MainPage.html', // Åbn MainPage.html som standard ved start
+  },
+});
